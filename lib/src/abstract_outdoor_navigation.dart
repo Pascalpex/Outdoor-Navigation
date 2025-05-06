6d1315327466d0a09b1421602b744ff3009f5ddb
Pascalpex <pascalpex@gmail.com>
2025-05-06 20:51:47 +0200

    Added documentation for important classes/methods

--- lib/src/abstract_outdoor_navigation.dart
+++ lib/src/abstract_outdoor_navigation.dart
@@ -1,5 +1,11 @@
 import 'package:latlong2/latlong.dart';
 
+/// The OutdoorNavigation class provides all outdoor related location services.
+///
+/// To obtain an instance use OutdoorNavigationProvider#getOutdoorNavigation.
 abstract class OutdoorNavigation {
+  /// Gives the latitude and longitude coordinates of the user.
+  ///
+  /// Will ask for permission on first usage.
   Future<LatLng?> getLocation();
 }
--- lib/src/outdoor_navigation_provider.dart
+++ lib/src/outdoor_navigation_provider.dart
@@ -1,7 +1,11 @@
 import 'package:outdoor_navigation/src/abstract_outdoor_navigation.dart';
 import 'package:outdoor_navigation/src/impl/outdoor_navigation_impl.dart';
 
+/// The OutdoorNavigationProvider provides a static method for getting an implementation of OutdoorNavigation.
+///
+/// See the getOutdoorNavigation Method of this class.
 class OutdoorNavigationProvider {
+  /// Returns an implementation of OutdoorNavigation.
   static OutdoorNavigation getOutdoorNavigation() {
     return OutdoorNavigationImpl();
   }
